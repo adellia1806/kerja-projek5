0591b1c63d12fa92564fc216af0160384f853f0d
adellia1806 <adelliazalfa24@gmail.com>
2023-11-24 13:57:46 +0700

    first commit

---
+++ src/aplikasi_restoran/Aplikasi_Restoran.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi_restoran;
+
+/**
+ *
+ * @author DELL
+ */
+public class Aplikasi_Restoran {
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args){
+        // TODO cod application logic here:
+       Login l = new Login();
+        l.setVisible(true);
+        
+    }
+}
